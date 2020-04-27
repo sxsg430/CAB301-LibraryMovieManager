48f90ea01dd859b00416caab9f8d97a5809d134f
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-04-27 21:38:47 +1000

    Add main menu

--- CAB302-LibraryMovieManager/Program.cs
+++ CAB302-LibraryMovieManager/Program.cs
@@ -8,8 +8,35 @@ namespace CAB302_LibraryMovieManager
 {
     class Program
     {
+        public static int MainMenuInterface ()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Community Library");
+            Console.WriteLine("===========Main Menu============");
+            Console.WriteLine("1. Staff Login");
+            Console.WriteLine("2. Member Login");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("================================");
+            Console.WriteLine("");
+            Console.WriteLine("Please make a selection (1-2, or 0 to exit): ");
+            int result = int.Parse(Console.ReadLine());
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            int mainMenuResult = MainMenuInterface();
+            if (mainMenuResult == 0)
+            {
+                
+            } else if (mainMenuResult == 1)
+            {
+                Console.WriteLine("Staff Login Here");
+            } else if (mainMenuResult == 2)
+            {
+                Console.WriteLine("User Login Here");
+            }
+            Console.ReadLine();
         }
     }
 }
