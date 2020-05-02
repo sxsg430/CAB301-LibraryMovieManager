572d77cb3ac433778e6d8bf9d64e2af77e21b9b7
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-05-02 19:55:08 +1000

    Add basic movie class

---
+++ CAB302-LibraryMovieManager/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAB302_LibraryMovieManager
+{
+    public class Movie
+    {
+        // Enums for Genre and Rating
+        public enum Genre
+        {
+            Drama,
+            Adventure,
+            Family,
+            Action,
+            SciFi, // "Sci-Fi" renamed to "SciFi" due to C# rules on using special characters ("-").
+            Comedy,
+            Animated,
+            Thriller,
+            Other
+        }
+        public enum Classification
+        {
+            General,
+            ParentalGuidance,
+            Mature,
+            MatureAccompanied
+
+        }
+
+        // Getters/Setters
+        public string MovieTitle { get; set; }
+        public string MovieStarring { get; set; }
+        public string MovieDirector { get; set; }
+        public string MovieDuration { get; set; }
+        public Genre MovieGenre { get; set; }
+        public Classification MovieRating { get; set; }
+        public int MovieCopies { get; set; }
+    }
+}
