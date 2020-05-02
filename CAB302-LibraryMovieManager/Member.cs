5647a380497a0cbc7a636cec45dbbaedf21a19f9
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-05-02 20:20:12 +1000

    Add initial Member class

---
+++ CAB302-LibraryMovieManager/Member.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAB302_LibraryMovieManager
+{
+    public class Member
+    {
+
+
+        public string MemberFirstName { get; set; }
+        public string MemberLastName { get; set; }
+        public string MemberAddress { get; set; }
+        public string MemberPhoneNumber { get; set; }
+        public int MemberPasscode { get; set; }
+        public string GetUsername()
+        {
+            return MemberLastName + MemberFirstName;
+        }
+    }
+}
