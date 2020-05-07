19a642449870f43e9e0e327d10044f770267513f
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-05-07 12:09:27 +1000

    Init first stages of MemberCollection

---
+++ CAB302-LibraryMovieManager/MemberCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAB302_LibraryMovieManager
+{
+    public class MemberCollection
+    {
+        private Member[] LibraryMembers = new Member[10]; // Limit to 10 members
+    }
+}
