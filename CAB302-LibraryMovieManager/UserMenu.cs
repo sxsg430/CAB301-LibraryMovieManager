356df5368abfb47370edb984c1cc87190deb5961
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-05-16 11:03:02 +1000

    Start work on User Menu

---
+++ CAB302-LibraryMovieManager/UserMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAB302_LibraryMovieManager
+{
+    class UserMenu
+    {
+        public static void UserMenuInit()
+        {
+            int result = UserMainMenu();
+        }
+        public static int UserMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(Globals.CurrentUser);
+            Console.WriteLine("===========User Menu============");
+            Console.WriteLine("1. Add a new movie DVD");
+            Console.WriteLine("2. Remove a movie DVD");
+            Console.WriteLine("3. Register a new Member");
+            Console.WriteLine("4. Find a registered member's phone number");
+            Console.WriteLine("0. Return to main menu");
+            Console.WriteLine("=================================");
+            Console.Write("Please make a selection (1-4 or 0 to return to main menu): ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
